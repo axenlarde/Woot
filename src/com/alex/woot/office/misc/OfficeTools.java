5a7a3e7464b96190db4b706c1326777b557d558f
axenlarde <Alexandre@NANALENO>
2018-05-13 00:15:01 +0200

    DID Ranges management in progress

--- src/com/alex/woot/office/misc/OfficeTools.java
+++ src/com/alex/woot/office/misc/OfficeTools.java
@@ -328,6 +328,7 @@ public class OfficeTools
 				//With this request we inject the item as much as the office's did ranges
 				for(int i=0; i<o.getDidRanges().size(); i++)
 					{
+					//We make a copy of the item to inject
 					TranslationPattern myTP = new TranslationPattern(templateTP.getName(),
 							templateTP.getDescription(),
 							templateTP.getRoutePartitionName(),
